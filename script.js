cfa82b098680d695d13f76770e8f178b0a8d71f7
Averde4162 <Angelloverde4162@gmail.com>
2021-11-18 19:17:41 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var click = 0
+function okayu(){
+    click += 1;
+    document.getElementById("okayuL").innerHTML = click;
+}
+
+var click = 0
+function korone(){
+    click += 1;
+    document.getElementById("koroneL").innerHTML = click;
+}
+
+var click = 0
+function onClick(){
+    click += 1;
+    document.getElementById("clicks").innerHTML = click;
+}
